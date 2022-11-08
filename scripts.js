c5153ad302fb067c784697e7690478bc0bd227d8
Guilherme Fabris <guilherme.fabris@grupoppar.local>
2022-11-08 16:41:01 -0300

    Second commit

--- scripts.js
+++ scripts.js
@@ -5,6 +5,10 @@ var question_two = document.getElementById('question-2');
 var question_three = document.getElementById('question-3');
 var question_four = document.getElementById('question-4');
 var question_five = document.getElementById('question-5');
+var question_six = document.getElementById('question-6');
+var question_seven = document.getElementById('question-7');
+var question_eight = document.getElementById('question-8');
+
 
 function storeAnswer(question_number, event){
     if(event.target.type === 'radio'){
@@ -16,18 +20,47 @@ function storeAnswer(question_number, event){
 
 question_one.addEventListener('click', function(event){
     storeAnswer(1, event)
+    nextQuestion(2)
+    growProgressBar('20%')
 })
 question_two.addEventListener('click', function(event){
     storeAnswer(2, event)
+    nextQuestion(3);
+    growProgressBar("30%");
 })
 question_three.addEventListener('click', function(event){
     storeAnswer(3, event)
+    nextQuestion(4);
+    growProgressBar("50%");
 })
 question_four.addEventListener('click', function(event){
     storeAnswer(4, event)
+    nextQuestion(5);
+    growProgressBar("65%");
 })
 question_five.addEventListener('click', function(event){
     storeAnswer(5, event)
+    nextQuestion(6);
+    growProgressBar("80%");
+})
+question_six.addEventListener('click', function(event){
+    storeAnswer(6, event)
+    nextQuestion(7);
+    growProgressBar("90%");
+})
+question_seven.addEventListener('click', function(event){
+    storeAnswer(7, event)
+    nextQuestion(8);
+    growProgressBar("100%");
+})
+question_eight.addEventListener('click', function(event){
+    storeAnswer(5, event)
+    nextQuestion(9);
+})
+
+submit8.addEventListener('click', function(){
+    document.getElementById("printtotalscore").innerHTML = totalScore();
+    document.getElementById("printscoreinfo").innerHTML = getInfoBasedOnScore();
 })
 
 function totalScore(){
@@ -35,17 +68,21 @@ function totalScore(){
     answers.question1+
     answers.question2+
     answers.question3+
-    answers.question4+ 
-    answers.question5;
+    answers.question4+
+    answers.question5+
+    answers.question6+
+    answers.question7+
+    answers.question8;
+   
     
     return total_score;
 }
 
 function getInfoBasedOnScore(){
     if(totalScore() < 7){
-        var score_info = "Você precisa tomar mais cuidado com a segurança!";
+        var score_info = "Você precisa se orientar mais sobre os cuidados com a raiva!";
     } else if(totalScore() > 7){
-        var score_info = "Parabéns! Você está bem de segurança!"
+        var score_info = "Parabéns! Você está ciente dos cuidados sobre a raiva!"
     }
 
     return score_info;
@@ -56,6 +93,10 @@ var submit2 = document.getElementById('submit2');
 var submit3 = document.getElementById('submit3');
 var submit4 = document.getElementById('submit4');
 var submit5 = document.getElementById('submit5');
+var submit6 = document.getElementById('submit6');
+var submit7 = document.getElementById('submit7');
+var submit8 = document.getElementById('submit8');
+
 
 function nextQuestion(question_number){
     var current_question_number = question_number - 1;
@@ -69,30 +110,10 @@ function nextQuestion(question_number){
     el2.style.display = "none";
 }
 
-submit1.addEventListener('click', function(){
-    nextQuestion(2);
-    growProgressBar('40%');
-})
-submit2.addEventListener('click', function(){
-    nextQuestion(3);
-    growProgressBar('60%');
-})
-submit3.addEventListener('click', function(){
-    nextQuestion(4);
-    growProgressBar('80%');
-})
-submit4.addEventListener('click', function(){
-    nextQuestion(5);
-    growProgressBar('100%');
-})
-submit5.addEventListener('click', function(){
-    nextQuestion(6);
-})
 
-submit5.addEventListener('click', function(){
-    document.getElementById("printtotalscore").innerHTML = totalScore();
-    document.getElementById("printscoreinfo").innerHTML = getInfoBasedOnScore();
-})
+
+
+
 
 function growProgressBar(percentage_width){
     var bar = document.getElementById("progress_bar");
